cc4aaae9d62d2beafb5fc315fb62b41e03a068e1
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-20 23:48:40 +0330

    hello React and Vite

---
+++ architectureplatform/vite.config.ts
@@ -0,0 +1,36 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'architectureplatform',
+      short_name: 'architectureplatform',
+      description: 'A PWA platform for architecture and interior design with projects, services, blog, and shop.',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
