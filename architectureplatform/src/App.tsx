31da4a1b37834282dd275e450b3d8ed6c1997091
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-21 19:44:28 +0330

    delete some file and update

--- architectureplatform/src/App.tsx
+++ architectureplatform/src/App.tsx
@@ -1,37 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import routes from "~react-pages";
+import PWABadge from "./PWABadge.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="architectureplatform logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>architectureplatform</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
-  )
+function AppRoutes() {
+  const element = useRoutes(routes);
+  return element;
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <PWABadge />
+      <div className="w-full min-h-screen flex flex-col bg-base-200">
+        <AppRoutes />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
