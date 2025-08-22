4596f8ed54d3e701cb3776a7cd03c2978f6d1c0c
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-22 23:57:04 +0330

    add react-toastify

--- architectureplatform/src/App.tsx
+++ architectureplatform/src/App.tsx
@@ -5,7 +5,8 @@ import Header from "./components/ui/header.tsx";
 import Footer from "./components/ui/footer.tsx";
 import FloatingElements from "./components/floatingElements.tsx";
 import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./components/ui/errorFallBack.tsx";
+import ErrorFallback from "./components/ui/errorFallback.tsx";
+import { ToastContainer } from "react-toastify";
 
 function AppRoutes() {
   const element = useRoutes(routes);
@@ -16,15 +17,14 @@ function App() {
   return (
     <BrowserRouter>
       <PWABadge />
-      <ErrorBoundary
-        FallbackComponent={ErrorFallback}
-      >
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <div className="w-full min-h-screen flex flex-col bg-base-200">
           <FloatingElements />
           <Header />
           <AppRoutes />
           <Footer />
         </div>
+        <ToastContainer />
       </ErrorBoundary>
     </BrowserRouter>
   );
